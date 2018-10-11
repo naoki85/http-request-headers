e9425595c1bce962d5ef4559e339cbd844c55bd5
naoki85 <naoki.yoneyama.85@gmail.com>
2018-10-11 23:55:01 +0900

    Add index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::API
+
+  def index
+    tmp_headers = request.headers.sort.map do |k, v|
+      hash = {}
+      next unless k.is_a?(String) && v.is_a?(String)
+      hash[k] = v
+      hash
+    end
+    tmp_env = request.env.sort.map do |k, v|
+      hash = {}
+      next unless k.is_a?(String) && v.is_a?(String)
+      hash[k] = v
+      hash
+    end
+    @headers = tmp_headers + tmp_env
+    render json: @headers
+  end
 end
