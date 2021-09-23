e5c4b3043a9df09c3fb5d4ea2e3d12212ea65a04
naoki85 <naoki.yoneyama.85@gmail.com>
2021-09-23 22:07:21 +0900

    remove env params from response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,12 @@
 class ApplicationController < ActionController::API
 
   def index
-    tmp_headers = request.headers.sort.map do |k, v|
+    @headers = request.headers.sort.map do |k, v|
       hash = {}
       next unless k.is_a?(String) && v.is_a?(String)
       hash[k] = v
       hash
-    end
-    tmp_env = request.env.sort.map do |k, v|
-      hash = {}
-      next unless k.is_a?(String) && v.is_a?(String)
-      hash[k] = v
-      hash
-    end
-    @headers = tmp_headers + tmp_env
+    end.compact
     render json: @headers
   end
 end
